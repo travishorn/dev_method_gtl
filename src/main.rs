72f6fbbedc2a5c7b5387c1a11ae6b83381efad06
Travis Horn <travis@travishorn.com>
2023-03-12 17:00:36 -0500

    Take by reference

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-// Instead of coding two identical `for` loops, we can remove duplication by writing it as a
-// function, then calling it each time we want to use it.
-fn largest(number_list: Vec<i32>) -> i32 {
+// We can be more memory efficient by taking `number_list` as a reference rather than taking
+// ownership over it. In the last version, an entire copy of the vector needed to be made. Since
+// we're not mutating `number_list` at any time, taking it by reference like you see here is
+// preferable.
+fn largest(number_list: &Vec<i32>) -> i32 {
     let mut largest = number_list[0];
 
-    for number in number_list {
+    for &number in number_list {
         if number > largest {
             largest = number;
         }
@@ -15,13 +17,13 @@ fn largest(number_list: Vec<i32>) -> i32 {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(number_list);
+    let result = largest(&number_list);
 
     println!("The largest number is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    let result = largest(number_list);
+    let result = largest(&number_list);
 
     println!("The largest number is {}", result);
 }
