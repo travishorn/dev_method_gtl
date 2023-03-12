148062232f7fecd35a2c3f2899f03deeeb357bbd
Travis Horn <travis@travishorn.com>
2023-03-12 17:27:10 -0500

    Restrict generic to PartialOrd

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-// We can reuse the same function for many types using a generic type. Warning: There is more work
-// to be done before this function works, however. Rust will complain that it cannot use the
-// comparison operator `>` here because it doesn't know how to compare the order of every possible
-// type you could give it.
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
+// Now, the generic type `T` given to `largest()` must implement the `PartialOrd` trait. This way,
+// Rust is guaranteed to know how to handle the comparison operator `>`.
 
-    for &item in list {
+// The return type must be `&T` now. It returns a reference to the largest element in the slice
+// rather than the element itself because moving a non-copy type out of a container like a slice is
+// not allowed.
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    // This variable must also be changed to a reference `&list[0]` because the function now returns
+    // a reference to the largest element, so it needs to keep track of a reference to the largest
+    // element instead of keeping track of the largest element's value itself.
+    let mut largest = &list[0];
+
+    // Finally, since the function now takes a reference to a slice, each item in the loop is
+    // already a reference to an element. We use `item` rather than `&item` because the latter would
+    // create a reference to the reference.
+    for item in list {
         if item > largest {
             largest = item;
         }
@@ -13,7 +21,6 @@ fn largest<T>(list: &[T]) -> T {
 
     largest
 }
-
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
