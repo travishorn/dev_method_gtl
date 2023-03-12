c22552e119ded6a351831d07a6b1c1eb8938ea3e
Travis Horn <travis@travishorn.com>
2023-03-12 16:58:51 -0500

    Remove duplication

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
+// Instead of coding two identical `for` loops, we can remove duplication by writing it as a
+// function, then calling it each time we want to use it.
+fn largest(number_list: Vec<i32>) -> i32 {
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let mut largest = number_list[0];
+    let result = largest(number_list);
 
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    println!("The largest number is {}", largest);
+    println!("The largest number is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    let mut largest = number_list[0];
+    let result = largest(number_list);
 
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    println!("The largest number is {}", largest);
+    println!("The largest number is {}", result);
 }
